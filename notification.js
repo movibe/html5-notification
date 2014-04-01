5a6c4d62761bba9e07438c0ab56da1eb7d29b7e8
Willian Ribeiro Angelo <agfoccus@gmail.com>
2014-04-01 14:38:27 -0300

    Melhorias

--- notification.js
+++ notification.js
@@ -1,24 +1,28 @@
-document.querySelector('.notify').addEventListener('click', function() {
-  'use strict';
+document.querySelector( '.notify' ).addEventListener( 'click', function () {
+	'use strict';
 
-  if (!("Notification" in window)) {
-    alert("Your browser doesn't support Notifications, but I brought this ooold alert for you :)")
-  } else if (Notification.permission === 'granted') {
-    var notification = new Notification('Notification API', {
-      icon: 'icon.png',
-      body: 'This is a HTML5 Notification :)'
-    });
-  } else {
-    Notification.requestPermission(function(permission) {
-      if (!('permission' in Notification)) {
-        Notification.permission = permission;
-      }
-      if (Notification.permission === 'granted') {
-        var notification = new Notification('Notification API', {
-          icon: 'icon.png',
-          body: 'This is a HTML5 Notification :)'
-        })
-      }
-    })
-  }
-})
+	var titulo = "Titulo da Mensagem";
+	var icone = 'icon.png';
+	var mensagem = 'Essa é uma notificação com HTML 5 :)';
+
+	if ( !( "Notification" in window ) ) {
+		alert( "Your browser doesn't support Notifications, but I brought this ooold alert for you :)" );
+	} else if ( Notification.permission === 'granted' ) {
+		var notification = new Notification( titulo, {
+			icon: icone,
+			body: mensagem
+		} );
+	} else {
+		Notification.requestPermission( function ( permission ) {
+			if ( !( 'permission' in Notification ) ) {
+				Notification.permission = permission;
+			}
+			if ( Notification.permission === 'granted' ) {
+				var notification = new Notification( titulo, {
+					icon: icone,
+					body: mensagem
+				} )
+			}
+		} )
+	}
+} )
